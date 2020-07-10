14504f4e06eda8341b09f8c5f60e7e31741a0e62
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-07-10 17:14:12 -0300

    created User model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import Router from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ status: 'Teste2' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

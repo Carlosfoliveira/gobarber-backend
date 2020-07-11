a4061d76a5a8df54b9cb3c98852d0564550a9db4
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-07-11 12:54:48 -0300

    Created the hash password and the middleware for authetication

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { id } = await jwt.verify(token, authConfig.secret);
+
+    req.userId = id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import Router from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;

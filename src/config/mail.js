93f492255f312f275d189fc9c817dda3dd0180e2
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2020-07-13 00:49:39 -0300

    Sending email to provider when cancel appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'cef5f9ead405f7',
+    pass: '6776a5f3297dc1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
